39a92537c483f8ca8f57ed64dd55476a50a2b967
ottjw <ottjw@yahoo.com>
2016-03-17 07:37:27 +0000

    memory game- needs more work

---
+++ memory/app/assets/javascripts/game.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+  var twoFlip = [];
+  var revealed = [];
+
+  function turnBack() {
+    $('.reveal').removeClass('reveal').addClass('text-box');
+    twoFlip = [];
+  }
+
+  function keep() {
+    $('.reveal').addClass('keep').removeClass('reveal text-box').unbind('click');
+    revealed.push(1);
+    win();
+  }
+
+  function win() {
+    if(revealed.length == 8) {
+      alert('You won!');
+    }
+  }
+
+
+  function check() {
+    if(twoFlip.length == 2) {
+      if(twoFlip[0] === twoFlip[1]) {
+        keep()
+        twoFlip = [];
+        alert('You found a match!')
+      } else {
+          alert("They don't match")
+          turnBack();
+          twoFlip = [];
+      }
+    }
+  }
+
+  $('.text-box').click(function() {
+    if( $(this).hasClass('reveal') ){
+      $(this).unbind('click');
+    } else {
+      var card = $(this).addClass('reveal').removeClass('text-box');
+      twoFlip.push(card.html());
+      check();
+    }
+  });
+
+  $('#play').click(function() {
+    if(confirm('Are you sure?')) {
+      $('.keep').addClass('text-box').removeClass('keep');
+      revealed = [];
+    }
+  });
+
+
+
+
+
+
+
+
+});
